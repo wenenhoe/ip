17fe55621da2fd9ada0c3fc829bf9cdd95f685e7
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-01-21 17:16:09 +0800

    style: Rename chatbot
    Give chatbot a new name to differentiate it from the placeholder name
    `Duke`

---
+++ src/main/java/Magus.java
@@ -0,0 +1,13 @@
+public class Magus {
+    public static void main(String[] args) {
+        String logo =
+                "  __  __               _____   _    _    _____ \n" +
+                " |  \\/  |     /\\      / ____| | |  | |  / ____|\n" +
+                " | \\  / |    /  \\    | |  __  | |  | | | (___  \n" +
+                " | |\\/| |   / /\\ \\   | | |_ | | |  | |  \\___ \\ \n" +
+                " | |  | |  / ____ \\  | |__| | | |__| |  ____) |\n" +
+                " |_|  |_| /_/    \\_\\  \\_____|  \\____/  |_____/ \n";
+        System.out.println(logo);
+        System.out.println("Hello I'm Magus");
+    }
+}
