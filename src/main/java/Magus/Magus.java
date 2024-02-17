d934d0fb63a11c7e7b971b83f80b8f34d262266c
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-02-17 16:58:23 +0800

    refactor: Move `Magus.processInput` to `CommandManager`

--- src/main/java/Magus/Magus.java
+++ src/main/java/Magus/Magus.java
@@ -1,11 +1,11 @@
 package Magus;
 
 import Magus.console.Command;
+import Magus.console.CommandManager;
 import Magus.console.Console;
 import Magus.console.Parser;
 import Magus.exception.CommandNotFoundException;
 import Magus.task.TaskManager;
-import Magus.task.TaskType;
 
 public class Magus {
     public static void main(String[] args) {
@@ -17,48 +17,13 @@ public class Magus {
             String input = Console.getUserInput();
             Parser parser = new Parser(input);
             try {
-                Magus.processInput(parser, taskManager);
+                CommandManager.processInput(parser, taskManager);
             } catch (CommandNotFoundException e) {
                 Console.printError(e);
             }
 
             // Exit when bye command issued
             isExitProgram = parser.getCommand() == Command.BYE;
-        }
-    }
-
-    public static void processInput(Parser parser, TaskManager taskManager)
-            throws CommandNotFoundException {
-        int taskNum;
-        Command command = parser.getCommand();
-        String additionalInput = parser.getAdditionalInput();
-
-        switch (command) {
-        case UNKNOWN:
-            throw new CommandNotFoundException();
-        case LIST:
-            taskManager.printTaskList();
-            break;
-        case BYE:
-            Console.printResponse("Bye. Hope to see you again soon!");
-            break;
-        case MARK:
-            taskNum = Parser.parseInt(additionalInput);
-            taskManager.markTaskAsDone(taskNum);
-            break;
-        case UNMARK:
-            taskNum = Parser.parseInt(additionalInput);
-            taskManager.unmarkTaskAsDone(taskNum);
-            break;
-        case TODO:
-            taskManager.addTask(TaskType.TODO, additionalInput);
-            break;
-        case DEADLINE:
-            taskManager.addTask(TaskType.DEADLINE, additionalInput);
-            break;
-        case EVENT:
-            taskManager.addTask(TaskType.EVENT, additionalInput);
-            break;
         }
     }
 }
---
+++ src/main/java/Magus/console/CommandManager.java
@@ -0,0 +1,42 @@
+package Magus.console;
+
+import Magus.exception.CommandNotFoundException;
+import Magus.task.TaskManager;
+import Magus.task.TaskType;
+
+public class CommandManager {
+    public static void processInput(Parser parser, TaskManager taskManager)
+            throws CommandNotFoundException {
+        int taskNum;
+        Command command = parser.getCommand();
+        String additionalInput = parser.getAdditionalInput();
+
+        switch (command) {
+        case UNKNOWN:
+            throw new CommandNotFoundException();
+        case LIST:
+            taskManager.printTaskList();
+            break;
+        case BYE:
+            Console.printResponse("Bye. Hope to see you again soon!");
+            break;
+        case MARK:
+            taskNum = Parser.parseInt(additionalInput);
+            taskManager.markTaskAsDone(taskNum);
+            break;
+        case UNMARK:
+            taskNum = Parser.parseInt(additionalInput);
+            taskManager.unmarkTaskAsDone(taskNum);
+            break;
+        case TODO:
+            taskManager.addTask(TaskType.TODO, additionalInput);
+            break;
+        case DEADLINE:
+            taskManager.addTask(TaskType.DEADLINE, additionalInput);
+            break;
+        case EVENT:
+            taskManager.addTask(TaskType.EVENT, additionalInput);
+            break;
+        }
+    }
+}
