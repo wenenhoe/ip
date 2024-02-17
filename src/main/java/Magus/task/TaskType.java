7f0d3b087d3226a715e69e9d4727bbfcaa10b592
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-02-17 18:28:56 +0800

    feat: Add get `TaskType` by task badge method
    Will be used when parsing stored task info

--- src/main/java/Magus/task/TaskType.java
+++ src/main/java/Magus/task/TaskType.java
@@ -3,5 +3,18 @@ package Magus.task;
 public enum TaskType {
     TODO,
     DEADLINE,
-    EVENT
+    EVENT;
+
+    public static TaskType getEnum(char badge) {
+        switch (badge) {
+        case 'D':
+            return TaskType.DEADLINE;
+        case 'E':
+            return TaskType.EVENT;
+        case 'T':
+            return TaskType.TODO;
+        default:
+            throw new IllegalStateException("Unexpected value: " + badge);
+        }
+    }
 }
