079e9353e4223861b30ebdc00ad8f596f4a6ad22
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-01 01:07:22 +0800

    docs: Add docs for `Todo`

--- src/main/java/magus/task/variant/Todo.java
+++ src/main/java/magus/task/variant/Todo.java
@@ -5,15 +5,33 @@ import magus.exception.ArgumentNotFoundException;
 import magus.exception.UnknownArgumentException;
 import magus.task.Task;
 
+import java.time.format.DateTimeParseException;
 import java.util.Map;
 
 import static magus.task.storage.Parser.DELIMITER;
 
+/**
+ * Task variant Todo that stores a description
+ */
 public class Todo extends Task {
+    /**
+     * Constructor for task variant Todo
+     *
+     * @param description Describe the todo task
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Parses the console input to construct Todo
+     *
+     * @param parser Console parser that parsed user input
+     * @return Todo object constructed from a console input
+     * @throws ArgumentNotFoundException No description found
+     * @throws UnknownArgumentException Unknown argument <code>/?</code> specified
+     * @see magus.console.Parser
+     */
     public static Todo parseConsoleTaskInfo(Parser parser)
             throws ArgumentNotFoundException, UnknownArgumentException {
         String descriptionCommand = "";
@@ -22,6 +40,14 @@ public class Todo extends Task {
         return new Todo(description);
     }
 
+    /**
+     * Parses the stored input to construct Todo
+     *
+     * @param description Description of Todo
+     * @param isDone Whether Todo is marked as done
+     * @return Todo object restored from a stored string
+     * @see #toStoredString()
+     */
     public static Todo parseStoredTaskInfo(String description, boolean isDone) {
         boolean hasNoDescription = description.isEmpty();
         if (hasNoDescription) {
