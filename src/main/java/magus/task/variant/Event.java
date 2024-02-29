e3e37271f7b020cadb99fb1d3ef3eae9fde9d9f0
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-01 00:48:25 +0800

    docs: Add docs for `Event`

--- src/main/java/magus/task/variant/Event.java
+++ src/main/java/magus/task/variant/Event.java
@@ -11,16 +11,37 @@ import java.util.Map;
 
 import static magus.task.storage.Parser.DELIMITER;
 
+/**
+ * Task variant Event that stores a description, the start date and end date
+ */
 public class Event extends Task {
     private final LocalDate start;
     private final LocalDate end;
 
+    /**
+     * Constructor for task variant Event
+     *
+     * @param description Describe the event
+     * @param start Start date for task event
+     * @param end End date for task event
+     */
     public Event(String description, LocalDate start, LocalDate end) {
         super(description);
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Parses the console input to construct Event
+     *
+     * @param parser Console parser that parsed user input
+     * @return Event object constructed from a console input
+     * @throws ArgumentNotFoundException <code>/from</code> and/or <code>/to</code> argument not found
+     * @throws DateTimeParseException Invalid date specified with <code>/from</code>
+     * and/or <code>/to</code> argument
+     * @throws UnknownArgumentException Unknown argument <code>/?</code> specified
+     * @see magus.console.Parser
+     */
     public static Event parseConsoleTaskInfo(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String descriptionCommand = "";
@@ -37,6 +58,14 @@ public class Event extends Task {
         return new Event(description, start, end);
     }
 
+    /**
+     * Parses the stored input to construct Event
+     *
+     * @param taskInfoSplit String array of each component to reconstruct Event
+     * @param isDone Whether Event is marked as done
+     * @return Event object restored from a stored string
+     * @see #toStoredString()
+     */
     public static Event parseStoredTaskInfo(String[] taskInfoSplit, boolean isDone) {
         if (taskInfoSplit.length != 3) {
             return null;
