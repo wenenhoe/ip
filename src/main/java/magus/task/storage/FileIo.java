8308c83ab1fba62e2e45988662c07a883ced41fb
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-02-29 17:45:54 +0800

    docs: Add docs for `FileIo`

--- src/main/java/magus/task/storage/FileIo.java
+++ src/main/java/magus/task/storage/FileIo.java
@@ -1,5 +1,7 @@
 package magus.task.storage;
 
+import magus.task.Task;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,11 +14,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Handles interactions with file system to read from and write to task list file
+ */
 public class FileIo {
     private static final String TASK_LIST_FILE_NAME = "Magus.txt";
     private static final String TASK_LIST_FILE_DIRECTORY = "./data/";
     private static final String TASK_LIST_FILE_PATH = TASK_LIST_FILE_DIRECTORY + TASK_LIST_FILE_NAME;
 
+    /**
+     * Reads task list file specified by the relative file path
+     *
+     * @return List of task command strings that can be parsed and
+     * converted back to their relevant class instances
+     * @see #TASK_LIST_FILE_PATH
+     */
     public static List<String> readTaskListFile() {
         List<String> taskCommandStrings = new ArrayList<>();
         File taskListFile = new File(TASK_LIST_FILE_PATH);
@@ -37,6 +49,14 @@ public class FileIo {
         return taskCommandStrings;
     }
 
+    /**
+     * Writes to task list file specified by the relative file path
+     *
+     * @param taskCommandStrings List of task command strings formatted by
+     *                           each of the task types
+     * @see Task#toStoredString()
+     * @see #TASK_LIST_FILE_PATH
+     */
     public static void writeTaskListFile(List<String> taskCommandStrings) {
         File taskListFile = new File(TASK_LIST_FILE_PATH);
         FileWriter fileWriter = null;
