2af12ee03b44ef4ff939a9a829eb4023d412f838
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-02-29 18:09:39 +0800

    docs: Add docs for task storage `Parser`

--- src/main/java/magus/task/storage/Parser.java
+++ src/main/java/magus/task/storage/Parser.java
@@ -4,7 +4,16 @@ import magus.task.TaskType;
 
 import java.util.regex.Pattern;
 
+/**
+ * Handles parsing of stored task info.
+ * Obtains the basic information needed to identify task type and status
+ * before it is handed off to the relevant task type to parsed for additional
+ * task info.
+ */
 public class Parser {
+    /**
+     * Delimiter for storing and parsing stored task info
+     */
     public static final String DELIMITER = "\t|\t";
     private static final String DELIMITER_REGEX = Pattern.quote(DELIMITER);
     private TaskType taskType;
@@ -12,6 +21,11 @@ public class Parser {
     private String taskInfo;
     private String[] splitTaskInfo;
 
+    /**
+     * Constructor for parsing stored task info to obtain
+     * task type, status and additional task info
+     * @param storedString String of stored task info
+     */
     public Parser(String storedString) {
         int maxSplitCount = 3; // Obtain the task badge and task completion status
         String[] storedStringSplit = storedString.split(DELIMITER_REGEX, maxSplitCount);
