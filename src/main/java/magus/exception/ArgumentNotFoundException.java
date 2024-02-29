5f8fccdf73b87d01af49f0c1e734b458d48a0cc3
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-02-29 17:31:12 +0800

    docs: Add docs for exception classes

--- src/main/java/magus/exception/ArgumentNotFoundException.java
+++ src/main/java/magus/exception/ArgumentNotFoundException.java
@@ -1,5 +1,8 @@
 package magus.exception;
 
+/**
+ * Exception thrown when argument required is not found
+ */
 public class ArgumentNotFoundException extends Exception {
     public ArgumentNotFoundException() {}
 
