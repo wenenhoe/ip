1192ca752fa2e21a7f3c9c01d9d2ae2534ff4f9c
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-04 01:09:40 +0800

    chore: Remove unused import

--- src/main/java/magus/task/TaskFinder.java
+++ src/main/java/magus/task/TaskFinder.java
@@ -10,7 +10,6 @@ import magus.task.variant.Todo;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
