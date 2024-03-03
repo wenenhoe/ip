6e7e62d7c44042d8c0b0774280c32021a407fcbd
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2024-03-03 15:10:06 +0800

    docs: Add docs for `TaskFinder`

--- src/main/java/magus/task/TaskFinder.java
+++ src/main/java/magus/task/TaskFinder.java
@@ -16,13 +16,33 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/**
+ * Finds relevant tasks to search filter
+ */
 public class TaskFinder {
     private final List<Task> taskList;
 
+    /**
+     * Constructor for TaskFinder that initialises the taskList
+     * @param taskList List of tasks to filter
+     */
     public TaskFinder(List<Task> taskList) {
         this.taskList = taskList;
     }
 
+    /**
+     * Filters tasks that matches the user search query.
+     * Attempt to filter by task type first and if failed,
+     * filter by description.
+     *
+     * @param parser Console parser that parsed user input
+     * @return List of task filtered from search query
+     * @see #filterTodos(Parser)
+     * @see #filterEvents(Parser)
+     * @see #filterDeadlines(Parser)
+     * @see #filterTasksByDate(Parser)
+     * @see #filterTasksByDescription(Parser, Parser, TaskType)
+     */
     public List<Task> filterTasks(Parser parser) {
         Parser subparser = new Parser(parser.getAdditionalInput());
         String taskTypeString = subparser.getCommandCandidate();
@@ -87,6 +107,16 @@ public class TaskFinder {
         return filteredTaskList;
     }
 
+    /**
+     * Filters tasks by checking if their description contain the search terms
+     *
+     * @param parser Console parser that parsed user input without task type
+     * @param subparser Console parser that parsed user input with task type
+     * @param taskType Task type to filter by
+     * @return List of task filtered from search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     */
     private List<Task> filterTasksByDescription(Parser parser, Parser subparser, TaskType taskType)
             throws UnknownArgumentException, ArgumentNotFoundException {
         String searchString = parser.parseAdditionalInput(true).get(""); // non-keyword arg
@@ -113,6 +143,15 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters tasks by checking if their date matches the search date
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterTasksByDate(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String dateCommand = "/date";
@@ -130,6 +169,14 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters Todo tasks by checking if their description contain the search terms
+     *
+     * @param parser Console parser that parsed user input containing
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterTodos(Parser parser)
             throws ArgumentNotFoundException, UnknownArgumentException {
         Map<String, String> parsedArgs = parser.parseAdditionalInput(true);
@@ -140,6 +187,15 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters Deadline tasks by checking if their date matches the search date
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterDeadlines(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String byCommand = "/by";
@@ -154,27 +210,45 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters Deadline tasks by checking if their date matches the search date
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @see #filterEventsByStartAndEnd(Parser)
+     * @see #filterEventsByStart(Parser)
+     * @see #filterEventsByEnd(Parser)
+     */
     private List<Task> filterEvents(Parser parser)
             throws DateTimeParseException {
-
         try {
             return filterEventsByStartAndEnd(parser);
-        } catch (ArgumentNotFoundException | UnknownArgumentException e) {
+        } catch (ArgumentNotFoundException | UnknownArgumentException ignored) {
             // continue the next filter type
         }
         try {
             return filterEventsByStart(parser);
-        } catch (ArgumentNotFoundException | UnknownArgumentException e) {
+        } catch (ArgumentNotFoundException | UnknownArgumentException ignored) {
             // continue the next filter type
         }
         try {
             return filterEventsByEnd(parser);
-        } catch (ArgumentNotFoundException | UnknownArgumentException e) {
+        } catch (ArgumentNotFoundException | UnknownArgumentException ignored) {
             // unknown (combination) of args
             return null;
         }
     }
 
+    /**
+     * Filters Event tasks by checking if their date matches the start and end search dates
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterEventsByStartAndEnd(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String fromCommand = "/from";
@@ -192,6 +266,15 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters Event tasks by checking if their date matches the start search dates only
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterEventsByStart(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String fromCommand = "/from";
@@ -204,6 +287,15 @@ public class TaskFinder {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Filters Event tasks by checking if their date matches the end search dates only
+     *
+     * @param parser Console parser that parsed user input containing search date
+     * @return List of task filtered from search query
+     * @throws ArgumentNotFoundException Expected argument not found in search query
+     * @throws DateTimeParseException Unable to parse date time specified in search query
+     * @throws UnknownArgumentException Unknown argument specified in search query
+     */
     private List<Task> filterEventsByEnd(Parser parser)
             throws ArgumentNotFoundException, DateTimeParseException, UnknownArgumentException {
         String toCommand = "/to";
